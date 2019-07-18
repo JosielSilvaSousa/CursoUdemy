639e504eb2a31e3113d9e00024ff785090d1ccdc
Josiel da Silva Sousa <josielsilva84@gmail.com>
2019-07-17 22:18:35 -0300

    Entidades

---
+++ Course/Course/Program.cs
@@ -0,0 +1,26 @@
+﻿using Course.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course.Entities.Enums;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Order order = new Order()
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+        }
+        
+
+    }
+}
